28956a8c92009323b691d0528204ede968d4f3d8
icebooda <tanginyourglass@gmail.com>
2014-10-13 23:48:01 -0400

    initial commit

--- src/monitorscript.pl
+++ src/monitorscript.pl
@@ -10,7 +10,7 @@ my $enable    = "MultiMonitorTool /enable $monnum";       #enable command
 my $disable   = "MultiMonitorTool /disable $monnum";      #disable command
 my $report    = "MultiMonitorTool.exe /scomma $file";     #report to show active monitors
 my $search    = "DISPLAY3";                               #search string to find the montior name
-my $mainsound = "Speaker";                                #name of main sound device
+my $mainsound = "Speakers";                               #name of main sound device
 my $tvsound   = "LG TV-4";                                #name of television sound device
 
 #enable and disable sound
@@ -40,12 +40,10 @@ foreach(@array){
     #if not active enable, else disable
     if($results[5] eq "No"){
       qx($enable);
-      qx($esound);
-      print $esound;
+      qx($dsound);
     }else{
       qx($disable);
-      qx($dsound);
-      print $dsound;
+      qx($esound);
     }
   }
 }
